31c9bf62bdaf44f052acafb258c5d950b4e74501
Gabriel <gabrielpmoreno8@gmail.com>
2021-04-18 01:51:36 -0700

    Removed debug messages

--- src/me/tx/guiapi/api/listeners/DecideConfirmListener.java
+++ src/me/tx/guiapi/api/listeners/DecideConfirmListener.java
@@ -1,20 +1,16 @@
 package me.tx.guiapi.api.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import me.tx.guiapi.api.events.DecideConfirmEvent;
 import me.tx.guiapi.api.interfaces.Confirmable;
-import net.md_5.bungee.api.ChatColor;
 
 public class DecideConfirmListener implements Listener {
-	
+
 	@EventHandler
 	public void onDecideConfirm(DecideConfirmEvent event) {
-		Bukkit.broadcastMessage(ChatColor.YELLOW + "This is a broadcast, the decide event was called. hi");
-		event.getConfirmMenu().getPlayer().sendMessage(ChatColor.RED + "decide event called.");
 		event.getConfirmMenu().getParentMenu().open();
-		((Confirmable)event.getConfirmMenu().getParentMenu()).onDecideConfirm(event);
+		((Confirmable) event.getConfirmMenu().getParentMenu()).onDecideConfirm(event);
 	}
 }
--- src/me/tx/guiapi/api/menus/ConfirmMenu.java
+++ src/me/tx/guiapi/api/menus/ConfirmMenu.java
@@ -11,7 +11,6 @@ import org.bukkit.inventory.ItemStack;
 import me.tx.guiapi.api.events.DecideConfirmEvent;
 import me.tx.guiapi.api.util.GeneralUtil;
 
-
 public class ConfirmMenu extends Menu {
 
 	public ConfirmMenu(Menu menu, String title, ItemStack item, int parentSlot) {
@@ -22,7 +21,7 @@ public class ConfirmMenu extends Menu {
 		build();
 		open();
 	}
-	
+
 	private final Menu parentMenu;
 
 	private final Inventory inventory = getInventory();
@@ -32,22 +31,19 @@ public class ConfirmMenu extends Menu {
 	private final ItemStack denyItem = GeneralUtil.makeItem(Material.RED_STAINED_GLASS_PANE, 1, "&4&lDeny");
 	private final int denySlot = 15;
 	private final int parentSlot;
-	
-	
+
 	@Override
 	public void build() {
 		IntStream.range(0, inventory.getSize()).forEach(i -> inventory.setItem(i, filler));
 		inventory.setItem(confirmSlot, confirmItem);
 		inventory.setItem(denySlot, denyItem);
 	}
-	
+
 	@Override
 	public void onClick(InventoryClickEvent event) {
 		if (event.getCurrentItem().isSimilar(filler)) {
-			event.getWhoClicked().sendMessage("SIMILAR");
 			return;
 		}
-		event.getWhoClicked().sendMessage("NOT SIMILAR");
 		Boolean decision = null;
 		switch (event.getSlot()) {
 		case confirmSlot:
@@ -60,16 +56,15 @@ public class ConfirmMenu extends Menu {
 			break;
 		}
 		if (decision != null) {
-			event.getWhoClicked().sendMessage("CALLING EVENT");
 			DecideConfirmEvent decideEvent = new DecideConfirmEvent(this, decision);
 			Bukkit.getPluginManager().callEvent(decideEvent);
 		}
 	}
-	
+
 	public Menu getParentMenu() {
 		return parentMenu;
 	}
-	
+
 	public int getParentSlot() {
 		return parentSlot;
 	}
